4b0f23af2c0244761c030c8489b8013d2855bf64
Paola <p.ably@hotmail.it>
2021-09-10 16:31:30 +0200

    plane height change

--- MultiplayerBodySwap/Assets/Scripts/PlaneHeight.cs
+++ MultiplayerBodySwap/Assets/Scripts/PlaneHeight.cs
@@ -6,15 +6,31 @@ using UnityEngine.XR.Interaction.Toolkit;
 [System.Serializable]
 public class PlaneHeight : MonoBehaviour
 {
-    public float playerHeight = 0;
-    public float avatarHeight = 1.7f;
+    public float player1Height = 0;
+    public float player2Height = 0;
+    public float avatar1Height = 1.7f;
+    public float avatar2Height = 1.7f;
+    public bool adaptToMaxOffset;
 
-    public void CalibrateHeight(float avatarHeight)
+    public void heightCalibration(float avatarHeight)
     {
-        this.avatarHeight = avatarHeight;
-        XRRig rig = FindObjectOfType<XRRig>();
-        playerHeight= rig.transform.Find("Camera Offset/Main Camera").position.y;
-        float offsetY = playerHeight - avatarHeight;
-        transform.position = new Vector3(0, offsetY, 0);
+        if (player1Height == 0)
+        {
+            this.avatar1Height = avatarHeight;
+            XRRig rig = FindObjectOfType<XRRig>();
+            player1Height = rig.transform.Find("Camera Offset/Main Camera").position.y;
+        }
+        else
+        {
+            this.avatar2Height = avatarHeight;
+            XRRig rig = FindObjectOfType<XRRig>();
+            player2Height = rig.transform.Find("Camera Offset/Main Camera").position.y;
+
+            //change plane height
+            float offsetY1 = player1Height - avatar1Height;
+            float offsetY2 = player2Height - avatar2Height;
+            float offset = (adaptToMaxOffset) ? Mathf.Max(offsetY1, offsetY2) : Mathf.Min(offsetY1, offsetY2);
+            transform.position = new Vector3(0, offset, 0);
+        }
     }
 }
