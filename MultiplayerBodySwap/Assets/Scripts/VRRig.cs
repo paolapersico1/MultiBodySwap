614d89d55fa37f586a8152ea104289ce985d92fd
Paola <p.ably@hotmail.it>
2021-10-08 16:43:04 +0200

    collision fix

--- MultiplayerBodySwap/Assets/Scripts/VRRig.cs
+++ MultiplayerBodySwap/Assets/Scripts/VRRig.cs
@@ -23,16 +23,16 @@ public class VRMap
 }
 public class VRRig : MonoBehaviour
 {
-    public float avatarHeight;
+    public bool collisionMode;
+
     public float avatarHeadHeight;
     public float avatarArmLength;
+
     public Vector3 avatarEyeHeadOffset;
-    public float torsoOffset;
     
     public VRMap head;
     public VRMap leftHand;
     public VRMap rightHand;
-    public Transform headTop;
 
     private Transform vrHead;
     private Transform vrLeftHand;
@@ -41,8 +41,11 @@ public class VRRig : MonoBehaviour
     public bool bodyRotation = true;
     public int rotThreshold;
     public float turnSmoothness;
-    public float handsTorsoRotation; //DEBUG
     public float crouchingThreshold;
+
+    //DEBUG
+    public float handsTorsoRotation;
+    public bool isCrouched;
 
     public Transform leftHandTarget;
     public Transform rightHandTarget;
@@ -59,37 +62,29 @@ public class VRRig : MonoBehaviour
         animator = GetComponent<Animator>();
 
         XRRig rig = FindObjectOfType<XRRig>();
-        vrHead = rig.transform.Find("Camera Offset/Main Camera");
-        vrLeftHand = rig.transform.Find("Camera Offset/LeftHand Controller/LeftHandCollider");
-        vrRightHand = rig.transform.Find("Camera Offset/RightHand Controller/RightHandCollider");
+        vrHead = rig.transform.Find("Player Offset/Camera Offset/Main Camera");
+        vrLeftHand = rig.transform.Find("Player Offset/Camera Offset/LeftHand Controller/LeftHandCollider");
+        vrRightHand = rig.transform.Find("Player Offset/Camera Offset/RightHand Controller/RightHandCollider");
 
         if (photonView.IsMine)
         {
-            Calibrate(GameObject.Find("XR Rig/Camera Offset"));
+            GameObject playerOffset = GameObject.Find("XR Rig/Player Offset");
+
+            float playerHeadHeight = playerOffset.GetComponent<Calibrator>().GetPlayerHeadHeight();
+            playerArmLength = playerOffset.GetComponent<Calibrator>().GetPlayerArmLength();
+
+            avatarHeadHeight = head.rigTarget.position.y - transform.position.y;
+            Debug.Log("Height difference: " + (avatarHeadHeight - playerHeadHeight));
+            playerOffset.transform.position = new Vector3(0, avatarHeadHeight - playerHeadHeight, 0);
+
+            //how longer are the avatar's arms
+            avatarArmLength = Vector3.Distance(rightHand.rigTarget.position, head.rigTarget.position);
+            float armOffset = avatarArmLength - playerArmLength;
+            leftHand.trackingPositionOffset = new Vector3(0, 0, armOffset);
+            rightHand.trackingPositionOffset = new Vector3(0, 0, armOffset);
+            leftHand.vrTarget.localPosition = leftHand.trackingPositionOffset;
+            rightHand.vrTarget.localPosition = rightHand.trackingPositionOffset;
         }
-    }
-
-    void Calibrate(GameObject camOffset)
-    {
-        float playerHeadHeight = camOffset.GetComponent<Calibrator>().GetPlayerHeadHeight();
-        playerArmLength = camOffset.GetComponent<Calibrator>().GetPlayerArmLength();
-
-        //how much taller is the avatar
-        avatarHeight = headTop.position.y - transform.position.y;
-
-        avatarHeadHeight = head.rigTarget.position.y - transform.position.y;
-        camOffset.transform.position = new Vector3(0, avatarHeadHeight - playerHeadHeight, 0);
-
-        //how longer are the avatar's arms
-        avatarArmLength = Vector3.Distance(rightHand.rigTarget.position, head.rigTarget.position);
-        float armOffset = avatarArmLength - playerArmLength;
-        leftHand.trackingPositionOffset = new Vector3(0, 0, armOffset);
-        rightHand.trackingPositionOffset = new Vector3(0, 0, armOffset);
-        vrLeftHand.localPosition = leftHand.trackingPositionOffset;
-        vrRightHand.localPosition = rightHand.trackingPositionOffset;
-
-        Transform cam = camOffset.transform.Find("Main Camera/Camera");
-        cam.localPosition = avatarEyeHeadOffset;
     }
 
     void OnAnimatorIK(int layerIndex)
@@ -98,7 +93,7 @@ public class VRRig : MonoBehaviour
         {
             if (vrRightHand)
             {
-                if (!HasCollided(vrRightHand))
+                if (!collisionMode || !HasCollided(vrRightHand))
                 {
                     Vector3 goalPosition = vrRightHand.TransformPoint(rightHand.trackingPositionOffset);
                     rightHandTarget.position = goalPosition;
@@ -109,7 +104,7 @@ public class VRRig : MonoBehaviour
             }
             if (vrLeftHand)
             {
-                if (!HasCollided(vrLeftHand))
+                if (!collisionMode || !HasCollided(vrLeftHand))
                 {
                     Vector3 goalPosition = vrLeftHand.TransformPoint(leftHand.trackingPositionOffset);
                     leftHandTarget.position = goalPosition;
@@ -138,6 +133,11 @@ public class VRRig : MonoBehaviour
     {
         if (photonView.IsMine)
         {
+            transform.position = new Vector3(head.rigTarget.position.x,
+                                IsCrouched(head.rigTarget.position) ? head.rigTarget.position.y - avatarHeadHeight :
+                                                                      transform.position.y,
+                                head.rigTarget.position.z);
+
             Vector3 handsPosition = Vector3.Lerp(rightHand.rigTarget.position, leftHand.rigTarget.position, 0.5f);
 
             handsTorsoRotation = Vector3.Angle(
@@ -152,11 +152,6 @@ public class VRRig : MonoBehaviour
                                             Time.deltaTime * turnSmoothness);
             }
 
-            transform.position = new Vector3(head.rigTarget.position.x,
-                                            IsCrouched(head.rigTarget.position) ? head.rigTarget.position.y - avatarHeadHeight :
-                                                                                  transform.position.y,
-                                            head.rigTarget.position.z) + transform.forward * torsoOffset;
-
             head.Map(vrHead);
         }
     }
@@ -169,7 +164,7 @@ public class VRRig : MonoBehaviour
         if(Physics.Raycast(ray, out hit))
         {
             float distance = Vector3.Distance(headPosition, hit.transform.position);
-            if (distance < (avatarHeight * crouchingThreshold))
+            if (distance < (avatarHeadHeight * crouchingThreshold))
                 return false;
         }
 
